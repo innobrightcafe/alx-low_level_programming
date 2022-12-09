c160e771622b70c67f474e8398dda4e87c2b82cb
innobrightcafe <innobrightcafe@gmail.co>
2022-12-09 23:21:54 +0100

    doubly linked lists

--- 0x17-doubly_linked_list/0-print_dlistint.c
+++ 0x17-doubly_linked_list/0-print_dlistint.c
@@ -1,26 +1,85 @@
-#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a
+ * dlistint_t list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count;
+
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
 
 /**
 
-print_dlistint - prints all the elements of a dlistint_t list
-
+print_dlistint - prints all the elements of a dlistint_t lists
 @h: pointer to the head of the list
 
 Return: the number of nodes in the list
 */
-size_t print_dlistint(const dlistint_t *h)
-{
-size_t count = 0;
 
-while (h)
+int main(void)
 {
-printf("%d\n", h->n);
-h = h->next;
-count++;
+	size_t print_dlistint(const dlistint_t *h)
+
+	size_t count = 0;
+
+	while (h)
+		printf("%d\n", h->n);
+	h = h->next;
+	count++;
+	return (count);
 }
 
-return (count);
+#include "lists.h"
 
+/**
+ * print_dlistint - prints all the elements of a
+ * dlistint_t list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count;
+
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
