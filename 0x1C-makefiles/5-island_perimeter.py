4028603f889542ba71a62997cd1ab6a39f6d470f
macbook <macbook@inosoft-MacBook-Pro.local>
2023-02-27 11:48:14 +0100

    the island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,41 +1,31 @@
 #!/usr/bin/python3
-from tkinter import Grid
+"""Module that calculates the perimeter of an island in a grid."""
+
+
+def num_water_neighbors(grid, i, j):
+    """Returns the number of water neighbors a cell has in a grid."""
+
+    num = 0
+
+    if i <= 0 or not grid[i - 1][j]:
+        num += 1
+    if j <= 0 or not grid[i][j - 1]:
+        num += 1
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+        num += 1
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
+        num += 1
+
+    return num
 
 
 def island_perimeter(grid):
-    """Returns the perimeter of the island described in the grid.
-    Args:
-- grid (list): a list of lists of integers representing the island.
+    """Returns the perimeter of the island in grid."""
 
-Returns:
-- perimeter (int): the perimeter of the island.
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                perimeter += num_water_neighbors(grid, i, j)
 
-Raises:
-- None
-
-"""
-
-# Get dimensions of the grid
-num_rows = len(grid)
-num_cols = len(grid[0])
-
-# Initialize perimeter
-perimeter = 0
-
-# Iterate over each cell in the grid
-for i in range(num_rows):
-    for j in range(num_cols):
-        if grid[i][j] == 1:
-            # Cell is part of the island
-            # Check neighbors to the left, right, top, and bottom
-            if j == 0 or Grid[i][j-1] == 0:
-                perimeter += 1
-            if j == num_cols-1 or grid[i][j+1] == 0:
-                perimeter += 1
-            if i == 0 or grid[i-1][j] == 0:
-                perimeter += 1
-            if i == num_rows-1 or grid[i+1][j] == 0:
-                perimeter += 1
-
-return perimeter
-
+    return perimeter
